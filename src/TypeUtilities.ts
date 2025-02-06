c732961a3cca6ad2fbe884f2ccbc5e24ace0dd07
eoghan <eoghan.firth@gmail.com>
2025-02-06 21:15:08 +0000

    return undefined after await

--- src/TypeUtilities.ts
+++ src/TypeUtilities.ts
@@ -61,7 +61,7 @@ export function createObjectWrapperWithCallbackRegistry<T>(
           ...path,
           prop,
         ]);
-        return reciever;
+        return undefined;
       }
 
       return createFunctionProxy(
